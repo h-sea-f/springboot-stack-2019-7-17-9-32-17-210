b32449e5c703e415cb405b6fc5d65a56121d3639
h-sea-f <h_sea_f@163.com>
2019-07-17 20:51:00 +0800

    finish story1

--- src/main/java/com/tw/apistackbase/repository/CaseRepository.java
+++ src/main/java/com/tw/apistackbase/repository/CaseRepository.java
@@ -3,5 +3,10 @@ package com.tw.apistackbase.repository;
 import com.tw.apistackbase.model.Case;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CaseRepository extends JpaRepository<Case,Integer> {
+    List<Case> findByoOrderByCaseNameDesc();
+    List<Case> findByCaseName();
+    Case deleteById();
 }
